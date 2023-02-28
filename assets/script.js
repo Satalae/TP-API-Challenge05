91888ca4ef47e5ac089b713e4846f1aad4014e1d
Satalae <harrison19988@gmail.com>
2023-02-28 14:30:05 -0800

    Time fixed, locally storing correct, display borked

--- assets/script.js
+++ assets/script.js
@@ -7,20 +7,25 @@ $(function () {
   var currentTime = dayjs().format('HH');
   var currentDate = dayjs().format('dddd, MMMM DD');
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-  //Listener for button presses
+  //Listener for save button presses
   $('.saveBtn').click(function() {
-    console.log(this.parentElement.siblings);
+    //first var grabs the parent div of the row
+    var parentPointer = this.parentElement;
+    console.log(parentPointer);
+    //second is split to ensure theres no dom traversal tomfoolery
+    var txtCont = $(parentPointer).children('textarea').val();
+
+    //Takes the ID number of the selected box
+    pointerID = $(parentPointer).attr("id");
+    numberID = pointerID.slice(5);
+    
+    //Stores input into local storage
+    localStorage.setItem("savedText" + numberID, txtCont);
   });
 
   //for loop to apply classes
   for(var i = 0; i < 24; i++){
+    //constructs the proper pointer for that hour
     var forTime = '#hour-' + i;
 
     if(i < currentTime){
@@ -42,16 +47,32 @@ $(function () {
   // attribute of each time-block be used to do this?
 
   //adds user input to local storage
-
+  function displayStorage(){
+    for(var i = 0; i < 23; i++){
+      //DOM Traversal to textarea, iterating through each time block
+      var boxSelect = $("hour-" + i).children('textarea');
+      console.log(boxSelect);
+      //Obtains the locally stored text for that given hour
+      var storedText = localStorage.getItem("savedText" + i);
+      console.log("The savedText is: " + storedText);
+      
+      //Sets inside box with locally stored text.
+      $(boxSelect).val(storedText);
+      console.log($(boxSelect).val());
+    }
+  }
   
   //Uses dayjs() to display current date in header
-  var currentDate = dayjs().format('dddd, MMMM DD');
-  var currentTime = dayjs().format('hh:mm:ss a');
-
   function displayTime(){
-    $('#currentDay').text(currentDate + '\n' + currentTime);
+    var dayNow = dayjs().format('dddd, MMMM DD');
+    var timeNow = dayjs().format('hh:mm:ss a');
+    $('#currentDay').text(dayNow + '\n' + timeNow);
   }
 
+  //Time being recalled on inverval
   displayTime();
   setInterval(displayTime, 1000);
+
+  //Run on pageload to fetch & display localStorage data
+  displayStorage();
 });
